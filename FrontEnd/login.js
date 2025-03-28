0b2f582c6e6b14046c54806681d9ac96af65259f
Nathan2IQ <nathankent196@gmail.com>
2025-03-28 11:43:01 +0100

    login fonctionnel

---
+++ FrontEnd/login.js
@@ -0,0 +1,42 @@
+//
+// je fais une fonction pour le login et verifier les infos
+//
+async function loginAuthentification() {
+    const loginForm = document.querySelector(".login__form");
+    
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const url = "http://localhost:5678/api/users/login";
+
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+            })
+            })
+            if(!response.ok) {
+                throw new Error("Cannot reach data");
+            }
+            const json = await response.json();
+            const token = json.token;
+            console.log(token);
+            localStorage.setItem("token", token);
+
+            window.location.href = "index.html";
+
+        } catch (error) {
+            console.error(error.message);
+        }
+    });
+}
+
+loginAuthentification();
